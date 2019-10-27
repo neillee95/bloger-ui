19df26a7b61c988d2f8970c41fd10704dc149fb3
lee <crossthebackstreet@gmail.com>
2019-10-28 00:17:34 +0800

    开发

--- src/views/admin/EditArticle.vue
+++ src/views/admin/EditArticle.vue
@@ -10,19 +10,22 @@
       <el-form-item label="分类" prop="category">
         <el-input v-model="form.category" maxlength="16" show-word-limit/>
       </el-form-item>
-      <el-form-item label="是否可以评论" prop="canComment">
+      <el-form-item label="封面图">
+        <el-input v-model="form.cover"/>
+      </el-form-item>
+      <el-form-item label="是否可以评论">
         <el-radio-group v-model="form.canComment">
           <el-radio :label="true">是</el-radio>
           <el-radio :label="false">否</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="是否发布" prop="publish">
+      <el-form-item label="是否发布">
         <el-radio-group v-model="form.publish">
           <el-radio :label="true">是</el-radio>
           <el-radio :label="false">否</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="即刻发布" prop="publishNow">
+      <el-form-item v-if="form.publish" label="即刻发布">
         <el-switch v-model="form.publishNow"></el-switch>
       </el-form-item>
       <el-form-item v-if="!form.publishNow" label="发布日期时间" prop="publishTime">
@@ -32,8 +35,7 @@
       </el-form-item>
       <el-form-item label="内容">
         <el-upload style="margin-bottom: 16px"
-                   action="https://baidu.com"
-                   :before-upload="beforeUpload">
+                   action="" :before-upload="beforeUpload">
           <el-button slot="trigger" size="small" type="primary" round><i class="el-icon-upload el-icon--right"></i> 上传文件</el-button>
           <div slot="tip" class="el-upload__tip">选择 <b>Markdown</b> 文件上传</div>
         </el-upload>
@@ -60,6 +62,7 @@
         form: {
           title: '',
           content: '',
+          cover: '',
           tags: '',
           category: '',
           canComment: true,
