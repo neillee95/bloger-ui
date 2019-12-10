05c6b61d93d2326ea83e9fdbbddb3449b7b5afb3
lee <crossthebackstreet@gmail.com>
2019-12-10 13:03:08 +0800

    custom TextDivider component

---
+++ src/components/TextDivider.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="divider">
+    <span class="text" :style="{color: textColor}">
+      {{text}}
+    </span>
+    <div class="line"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TextDivider",
+    props: {
+      text: {
+        type: String,
+        required: true
+      },
+      'text-color': {
+        type: String,
+        default: '#b0baba'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .divider {
+    margin: 16px 0;
+    display: flex;
+    align-items: center;
+  }
+
+  .text {
+    padding: 4px 8px;
+    min-width: 32px;
+  }
+
+  .line {
+    width: 100%;
+    height: 1px;
+    background-color: #b0baba;
+  }
+</style>
